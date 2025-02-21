a2abf978afa66fc0ce16e7e42a81ed37ef5b7677
mactavishz <maczhanchao@yahoo.com>
2025-02-21 17:56:28 +0100

    fix: sync solutions with upstream changes

--- exercises/02_basic_calculator/03_if_else/src/lib.rs
+++ exercises/02_basic_calculator/03_if_else/src/lib.rs
@@ -1,9 +1,13 @@
-/// Return `true` if `n` is even, `false` otherwise.
-fn is_even(n: u32) -> bool {
+/// Return `12` if `n` is even,
+/// `13` if `n` is divisible by `3`,
+/// `17` otherwise.
+fn magic_number(n: u32) -> u32 {
     if n % 2 == 0 {
-        true
+        12
+    } else if n % 3 == 0 {
+        13
     } else {
-        false
+        17
     }
 }
 
