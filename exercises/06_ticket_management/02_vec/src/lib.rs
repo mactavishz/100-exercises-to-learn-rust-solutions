ae7b3ad05ac72a88821ea7d3cb0146208716e039
mactavishz <maczhanchao@yahoo.com>
2025-03-28 15:48:59 +0100

    add solution to 06_02

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,11 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut fibs = vec![0, 1];
+    while n as usize > fibs.len() - 1 {
+        fibs.push(fibs[fibs.len() - 1] + fibs[fibs.len() - 2])
+    }
+    return fibs[n as usize];
 }
 
 #[cfg(test)]
