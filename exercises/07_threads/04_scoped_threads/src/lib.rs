8a42724f06a1ffd056f6d69d9b9a326445f971e5
mactavishz <maczhanchao@yahoo.com>
2025-04-03 13:36:24 +0200

    add solution to 07_04

--- exercises/07_threads/04_scoped_threads/src/lib.rs
+++ exercises/07_threads/04_scoped_threads/src/lib.rs
@@ -3,7 +3,12 @@
 //  Don't perform any heap allocation. Don't leak any memory.
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let mid = v.len() / 2;
+    std::thread::scope(|scope| {
+        let l_sum: i32 = scope.spawn(|| v[..mid].iter().sum()).join().unwrap();
+        let r_sum: i32 = scope.spawn(|| v[mid..].iter().sum()).join().unwrap();
+        l_sum + r_sum
+    })
 }
 
 #[cfg(test)]
